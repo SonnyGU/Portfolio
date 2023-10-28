452f311c7067cbc76cbbf6fdd7fc90e21e87a81d
Guson Ulysse <Gulysse@wgu.edu>
2023-10-28 16:08:53 -0400

    updates text on screen

--- homepage/static/homepage/js/script.js
+++ homepage/static/homepage/js/script.js
@@ -4,7 +4,7 @@ function runIntro(){
     if(instructionCon){
         instructionCon.style.display = 'none';
     }
-    const introText = "Hi I'm Sonny, and this is my portfolio Determined Certified Pharmacy Technician/Data " +
+    const introText = "Hi I'm Sonny, and this is my portfolio. I'm a determined Certified Pharmacy Technician/Data " +
         "Coordinator transitioning into a dynamic Software Engineer role, backed by a B.S. in Computer Science. With a " +
         "keen analytical acumen and strong programming skills, I’m ready to contribute significantly to a progressive " +
         "development team. Committed to driving innovation, optimizing solutions, and embracing new challenges in " +
